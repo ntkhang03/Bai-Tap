8057214e89ca41b842353f7366fabf79c8ed51d3
NTKhang <81167726+ntkhang03@users.noreply.github.com>
2022-03-29 18:19:11 +0700

    Create xuất hashTag trong file blog.cs

---
+++ Lập trình nâng cao/xuất hashTag trong file blog.cs
@@ -0,0 +1,32 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace CSharp_Shell {
+
+  public static class Program {
+	  public static void Main() {
+	  	// đọc file blog.txt và xuất ra tất cả hashTag vào file Hashtag.txt
+	    StreamReader baiBao = new StreamReader("blog.txt");
+	    StreamWriter writeHashtag = new StreamWriter("Hashtag.txt");
+	    string line;
+	    string outputHashtag = "";
+	    
+	    // dùng foreach + IndexOf
+      while ((line = baiBao.ReadLine()) != null) {
+        string[] splitLine = line.Split(); // tách chuỗi bằng ký tự khoảng cách
+		    foreach (string hashTag in splitLine) // foreach duyệt từng phần tử của mảng, tài liệu tham khảo: https://howkteam.vn/course/khoa-hoc-lap-trinh-c-can-ban/vong-lap-foreach-trong-lap-trinh-c-co-ban-1193
+		      if (hashTag.IndexOf('#') == 0) outputHashtag += $"{hashTag}\n"; // nếu có ký tự # đứng ở vị trí đầu tiên => là hashTag => cộng thêm vào biến outputHashtag
+	    }
+	    
+	    writeHashtag.Write(outputHashtag); // viết nội dung trong biến outputHashtag vào file Hashtag.txt
+	    writeHashtag.Close(); 
+	    baiBao.Close();
+	    
+	    Console.WriteLine(outputHashtag); // xuất nội dung biến outputHashtag
+	    Console.WriteLine(outputHashtag); // xuất nội dung biến outputHashtag
+	  }
+  }
+}
