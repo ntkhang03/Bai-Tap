5a876e99c483b8926ec52773af33fd99c0a6cf54
NTKhang <81167726+ntkhang03@users.noreply.github.com>
2022-03-29 19:25:26 +0700

    Update main.cs

--- Lập trình nâng cao/main.cs
+++ Lập trình nâng cao/main.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Text.RegularExpressions;
 
 namespace BaiTap {
-
+  
   public static class Program {
     public static void Main(string[] args) {
       // Đề bài: đọc file blog.txt và xuất ra tất cả hashTag vào file Hashtag.txt
@@ -18,7 +18,7 @@ namespace BaiTap {
         foreach (string hashTag in splitLine) // foreach duyệt từng phần tử của mảng, tài liệu tham khảo: https://howkteam.vn/course/khoa-hoc-lap-trinh-c-can-ban/vong-lap-foreach-trong-lap-trinh-c-co-ban-1193
         
           // dùng foreach + IndexOf tìm hashtag trong hàng
-	        if (hashTag.IndexOf('#') == 0) writeHashtag.WriteLine(hashTag); // nếu có ký tự # đứng ở vị trí đầu tiên => là hashTag => viết vào file
+          if (hashTag.IndexOf('#') == 0) writeHashtag.WriteLine(hashTag); // nếu có ký tự # đứng ở vị trí đầu tiên => là hashTag => viết vào file
       }
 	    
       writeHashtag.Close();
