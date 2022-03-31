e6e975f364e18d26bf4fdfb43c97e3947846421e
ntkhanggggg <84844986+ntkhanggggg@users.noreply.github.com>
2022-03-31 10:36:31 +0700

    Create 31-03-2022.cs

---
+++ Cơ sở lập trình/31-03-2022.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap diem Toan: ");
+            float toan = float.Parse(Console.ReadLine());
+            Console.Write("Nhap diem Tin Hoc: ");
+            float tin = float.Parse(Console.ReadLine());
+            Console.Write("Nhap diem Anh Van: ");
+            float anhVan = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Loai: " + xepLoai(toan, tin, anhVan));
+        }
+
+        static string xepLoai(float toan, float tin, float anhVan)
+        {
+            float diemTB = (toan + tin + anhVan)/3;
+            if (diemTB >= 8 && toan >= 8 && tin >= 8 && anhVan >= 8)
+                return "gioi";
+            else if (diemTB >= 7 && toan >= 7 && tin >= 7 && anhVan >= 7)
+                return "kha";
+            else if (diemTB >= 6)
+                return "trung binh";
+            else
+                return "kem";
+        }
+    }
+}
